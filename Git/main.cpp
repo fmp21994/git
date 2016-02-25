28f0e1b424c39689b3f11c4c135e40e75d9311d0
Frank Palmisano <fmp21994@me.com>
2016-02-25 12:20:10 -0700

    Initial Commit

---
+++ Git/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Git
+//
+//  Created by Frank Palmisano on 2/25/16.
+//  Copyright © 2016 Frank Palmisano. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
